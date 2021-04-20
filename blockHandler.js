b1236706878ab181e673dbb1b3f17fd54c0c9bf8
Throphet <throphet@gmail.com>
2021-04-20 16:35:28 +0300

    quickfix II

--- blockHandler.js
+++ blockHandler.js
@@ -9,15 +9,14 @@ document.addEventListener("DOMContentLoaded", function(){
 
     let today = new Date();
     let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
     let yyyy = today.getFullYear();
 
     today = dd + '.' + mm + '.' + yyyy;
 
-
-
-
     let deleteCard = document.getElementsByClassName("delete");
+
+    reload(); // Delete buttons loaded
 
     CARD_ADD.addEventListener("click", function(){
         if(CARD_FORM.style.display === "none"){
